b05be000b07d1e31597ffc456b8a2d2d27c019fa
feiandxs <yiziyint@gmail.com>
2021-10-08 17:46:03 +0800

    first things first

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>test</div>
+  <div class='title'>在线GIF图片帧拆分工具</div>
+  <div>11</div>
 </template>
 <script>
 export default {
@@ -7,5 +8,7 @@ export default {
 }
 </script>
 <style scoped>
-
+  .title {
+    font-size: 24px;
+  }
 </style>
